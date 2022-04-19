97eee6ca49c405b2e1fbdcd8fda6e19d159027bd
Mkaell <mrohacevich@yandex.ru>
2022-04-19 09:51:55 +0300

    changed api key

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -7,7 +7,7 @@ interface IApiConfig {
 
 const apiConfig: IApiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: '2129af6ad8e13414069c9da4e190cbeb',
+    apiKey: '68bf2f1e148e557ae422380ea108deb9',
     w500Image: (posterPath) => `https://image.tmdb.org/t/p/w500/${posterPath}`,
     originalImage: (posterPath) => `https://image.tmdb.org/t/p/original/${posterPath}`
 }
